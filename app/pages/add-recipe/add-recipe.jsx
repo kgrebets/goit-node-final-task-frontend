888fc2d5da02e7e6a9cbd5941206750d1f84ef00
seliverstova-valerya <seliverstova.valerya@gmail.com>
2025-12-21 14:41:08 +0200

    fix: align the file names

--- app/pages/add-recipe/add-recipe.jsx
+++ app/pages/add-recipe/add-recipe.jsx
@@ -1,35 +1,29 @@
 import MainTitle from '../../components/ui/main-title';
 import Subtitle from '../../components/ui/subtitle';
 import PathInfo from '../../components/ui/path-info';
-import RecipeForm from '../../components/add-recipe/addRecipeForm';
-
-import '../../css/add-recipe.css'
-
+import RecipeForm from '../../components/add-recipe/add-recipe-form.jsx';
+import '../../css/add-recipe.css';
 
 export default function AddRecipePage() {
-    return (
+  return (
+    <section className="add-recipe-page">
+      <div className="add-recipe-page-container">
+        <PathInfo current="Add Recipe" />
+        <div className="add-recipe-page-content">
+          <div className="add-recipe-left-side">
+            <MainTitle>Add Recipe</MainTitle>
 
-       
-        <section className="add-recipe-page">
-            <div className="add-recipe-page-container">
-                <PathInfo current="Add Recipe" />
-                <div className="add-recipe-page-content">
+            <Subtitle>
+              Reveal your culinary art, share your favorite recipe and create
+              gastronomic masterpieces with us.
+            </Subtitle>
+          </div>
 
-                    <div className="add-recipe-left-side">
-                        <MainTitle>Add Recipe</MainTitle>
-
-                        <Subtitle>Reveal your culinary art, share your favorite recipe and create gastronomic masterpieces with us.</Subtitle>
-                    
-
-                    </div>
-
-                    
-                    <div className="add-recipe-right-side">
-                        <RecipeForm />
-                        </div>
-                </div>
-
-                </div>
-        </section>
-    );
+          <div className="add-recipe-right-side">
+            <RecipeForm />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
