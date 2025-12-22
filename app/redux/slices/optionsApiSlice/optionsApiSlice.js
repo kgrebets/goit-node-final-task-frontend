d68fdffb0edfb422e8c2d8085270c8a9548f882b
Artem Bilko <artembilko@MacBook-Pro-Artem.local>
2025-12-22 14:18:00 +0200

    Add base url

--- app/redux/slices/optionsApiSlice/optionsApiSlice.js
+++ app/redux/slices/optionsApiSlice/optionsApiSlice.js
@@ -1,15 +1,15 @@
-import { createApi, fakeBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import AreasApi from '../../../api-client/src/api/AreasApi';
 import CategoriesApi from '../../../api-client/src/api/CategoriesApi';
 import IngredientsApi from '../../../api-client/src/api/IngredientsApi';
-
+import ApiClient from '../../../api-client/src/ApiClient';
 const ingredientsApi = new IngredientsApi();
 const areasApi = new AreasApi();
 const categoriesApi = new CategoriesApi();
 
 export const optionsApi = createApi({
   reducerPath: 'optionsApi',
-  baseQuery: fakeBaseQuery(),
+  baseQuery: fetchBaseQuery({ baseUrl: ApiClient.instance.basePath }),
   endpoints: (builder) => ({
     getIngredientsList: builder.query({
       async queryFn({ page = 1, limit = Number.MAX_SAFE_INTEGER } = {}) {
